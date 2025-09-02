12140e494f5602c6ffd0377da7e05de9140a2b5f
mouna-ben-mansour <benmansour.mouna2@gmail.com>
2025-09-02 16:42:17 +0200

    loginScreen Binding

--- FacturationApp/Views/LoginView.xaml.cs
+++ FacturationApp/Views/LoginView.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace FacturationApp.View
+namespace FacturationApp.Views
 {
     /// <summary>
     /// Logique d'interaction pour LoginView.xaml
