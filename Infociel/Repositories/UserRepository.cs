2f46e7b67d2ded6064ccf545985c17cfe12ff836
mouna-ben-mansour <benmansour.mouna2@gmail.com>
2025-09-03 17:18:25 +0200

    change app namespace

---
+++ Infociel/Repositories/UserRepository.cs
@@ -0,0 +1,87 @@
+﻿using Infociel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infociel.Repositories
+{
+    public class UserRepository : RepositoryBase, IUserRepository
+    {
+        public void Add(UserModel userModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AuthenticateUser(NetworkCredential credential)
+        {
+            bool validUser;
+            Console.WriteLine("🔍 Authenticating user...");
+            Console.WriteLine($"👤 Username: {credential.UserName}");
+            using (var connection = GetConnection())
+            using (var command = new MySqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "select * from `user` where Username=@username and Password=@password";
+                command.Parameters.Add("@username", MySqlDbType.VarChar).Value = credential.UserName;
+                command.Parameters.Add("@password", MySqlDbType.VarChar).Value = credential.Password;
+                validUser = command.ExecuteScalar() == null ? false : true;
+            }
+            return validUser;
+        }
+
+        public void Edit(UserModel userModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserModel> GetByAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel GetByUsername(string username)
+        {
+            UserModel user = null;
+            using (var connection = GetConnection())
+            using (var command = new MySqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "select * from  `user` where Username=@username";
+                command.Parameters.Add("@username", MySqlDbType.VarChar).Value = username;
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        user = new UserModel()
+                        {
+                            Id = reader[0].ToString(),
+                            Username = reader[1].ToString(),
+                            Password = string.Empty,
+                            Name = reader[3].ToString(),
+                            LastName = reader[4].ToString(),
+                            Email = reader[5].ToString(),
+                        };
+                    }
+                }
+            }
+            return user;
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
